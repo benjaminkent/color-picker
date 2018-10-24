639e8f26af9d8c523cb7475becdeae1ddf561dea
Benjamin JEHL <benjaminjehl@Benjamins-MBP.fios-router.home>
2018-10-23 22:18:31 -0400

    updated colors

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 // import logo from './logo.svg';
-import './App.css';
+import './App.css'
 
 class App extends Component {
-
   constructor(props) {
     super(props)
 
     this.state = {
-      hueValue: 214,
-      saturationValue: 94,
+      hueValue: 150,
+      saturationValue: 68,
       lightnessValue: 48
     }
   }
@@ -48,50 +47,76 @@ class App extends Component {
   render() {
     return (
       <div>
-        <header>
+        <header className="header">
           <h1>Color Picker!</h1>
         </header>
 
         <main className="all-content">
-
           <aside>
             <div className="values">
-              <p className="hslValue" style={{ color: `hsl(${this.state.hueValue}, ${this.state.saturationValue}%, ${this.state.lightnessValue}%)` }}>H:{this.state.hueValue} S:{this.state.saturationValue}% L:{this.state.lightnessValue}%</p>
+              <p
+                className="hslValue"
+                style={{
+                  color: `hsl(${this.state.hueValue}, ${
+                    this.state.saturationValue
+                  }%, ${this.state.lightnessValue}%)`
+                }}
+              >
+                H:
+                {this.state.hueValue} S:
+                {this.state.saturationValue}% L:
+                {this.state.lightnessValue}%
+              </p>
             </div>
 
-            <div className="color-window" style={{ backgroundColor: `hsl(${this.state.hueValue}, ${this.state.saturationValue}%, ${this.state.lightnessValue}%)` }}>
-            </div>
+            <div
+              className="color-window"
+              style={{
+                backgroundColor: `hsl(${this.state.hueValue}, ${
+                  this.state.saturationValue
+                }%, ${this.state.lightnessValue}%)`
+              }}
+            />
           </aside>
 
           <div>
             <ul>
               <li>
                 <p>Hue</p>
-                <input onChange={this._moveHue} type="range" min="0" max="360" value={this.state.hueValue} />
+                <input
+                  onChange={this._moveHue}
+                  type="range"
+                  min="0"
+                  max="360"
+                  value={this.state.hueValue}
+                />
               </li>
               <li>
                 <p>Saturation</p>
-                <input onChange={this._moveSat} type="range" min="0" max="100" value={this.state.saturationValue} />
+                <input
+                  onChange={this._moveSat}
+                  type="range"
+                  min="0"
+                  max="100"
+                  value={this.state.saturationValue}
+                />
               </li>
               <li>
                 <p>Lightness</p>
-                <input onChange={this._moveLight} type="range" min="0" max="100" value={this.state.lightnessValue} />
+                <input
+                  onChange={this._moveLight}
+                  type="range"
+                  min="0"
+                  max="100"
+                  value={this.state.lightnessValue}
+                />
               </li>
             </ul>
           </div>
-
         </main>
-      </div >
-    );
+      </div>
+    )
   }
 }
 
-export default App;
-
-
-
-
-
-
-
-
+export default App
